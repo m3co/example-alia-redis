98d0b262eaa080dacd4c590101ad521a4633fc38
Andres Rios <rianby64@gmail.com>
2019-01-05 10:40:21 +0300

    Test a bit more the funciton ListenAndServe

--- aliaredis/start_test.go
+++ aliaredis/start_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net"
 	"testing"
+	"time"
 )
 
 func Test_Serve_Start_with_error(t *testing.T) {
@@ -33,4 +34,29 @@ func Test_Serve_Start_with_error(t *testing.T) {
 	if fmt.Sprint(err) != expectedError {
 		t.Error("they differ")
 	}
+}
+
+func Test_Serve_Start_send_one_command_and_exit(t *testing.T) {
+
+	// setup
+	s := Server{}
+	go s.ListenAndServe("")
+	time.Sleep(10 * time.Millisecond)
+
+	expectedRes := fmt.Sprintf("%q", "OK")
+	conn, err := net.Dial("tcp", ":3000")
+	if err != nil {
+		t.Error("something went wrong", err)
+		return
+	}
+
+	conn.Write([]byte("set key value\n"))
+	res := make([]byte, 4)
+	conn.Read(res)
+
+	if string(res) != expectedRes {
+		t.Error("expecting OK from server", string(res), expectedRes)
+	}
+	conn.Close()
+	s.Close()
 }
